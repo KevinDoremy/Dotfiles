67563cacb456c597075a0b18ff81f18b1f8142e7
Kevin Doremy <65405378+KevinDoremy@users.noreply.github.com>
2022-11-22 03:08:47 +0000

    Nov 21, 2022, 10:08 PM - ⭐

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -769,3 +769,14 @@ if (No-Module Terminal-Icons) {
         Install-Module -Name Terminal-Icons -Scope AllUsers -Force -SkipPublisherCheck
     }
 }
+
+$p = 0
+1..100 | ForEach-Object {
+    Write-Progress -Id 1 -Activity 'pwsh' -PercentComplete $_
+    $p += (Measure-Command {
+        pwsh -noprofile -command 1
+    }).TotalMilliseconds 
+}
+Write-Progress -id 1 -Activity 'profile' -Completed
+$p = $p/100
+$p
