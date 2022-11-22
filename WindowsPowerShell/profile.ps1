9ba35cb68b6413f9e12973100a7affeabc8fe9c7
Kevin Doremy <65405378+KevinDoremy@users.noreply.github.com>
2022-11-22 03:21:58 +0000

    Nov 21, 2022, 10:21 PM - ⭐

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -16,7 +16,8 @@ function make-link ($target, $link) {
 
 oh-my-posh --init --shell pwsh --config C:\Users\Kevin\Documents\unicorn.omp.json | Invoke-Expression
 Import-Module -Name Terminal-Icons
-Import-Module PSColors
+# Import-Module PSColors
+# Import-Module PSScriptAnalyzer
 Import-Module posh-git
 Import-Module -Name Terminal-Icons
 Invoke-Expression (& { (lua C:\Users\Kevin\Documents\z.lua --init powershell) -join "`n" })
