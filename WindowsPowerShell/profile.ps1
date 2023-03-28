e37288f708393a7f33f7f727d24e1bbd8ffacc7b
Kevin Doremy <65405378+KevinDoremy@users.noreply.github.com>
2023-03-28 14:32:46 +0000

    Mar 28, 2023, 10:32 AM

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -182,7 +182,7 @@ public class Fonts {
     }
 }
 
-Invoke-Expression (& { (lua ~/Documents/WindowsPowerShell/lua/z.lua --init powershell) -join "`n" })
+Invoke-Expression (& { (lua ~/Documents/WindowsPowerShell/Lua/z.lua --init powershell) -join "`n" })
 
 # https://github.com/skywind3000/z.lua 
 
