05b10128403a1ab611a6fb8eebca8a90428707ef
Kevin Doremy <65405378+KevinDoremy@users.noreply.github.com>
2023-04-15 01:52:49 +0000

    Apr 14, 2023, 9:52 PM

---
+++ python.py
@@ -0,0 +1,57 @@
+# Chess game without Gui or import 
+
+class Board:
+    def __init__(self):
+        self.board = [
+            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+        ]
+
+    def print_board(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                print(self.board[i][j], end=' ')
+            print() 
+
+    def move(self, start, end):
+        self.board[end[0]][end[1]] = self.board[start[0]][start[1]]
+        self.board[start[0]][start[1]] = ' '
+
+    def check_move(self, start, end):   
+        if self.board[end[0]][end[1]] != ' ':
+            return False
+        else:
+            return True
+
+    def check_king(self, start, end):
+        if self.board[end[0]][end[1]] == 'k':
+            return True
+        else:
+            return False
+
+    def check_pawn(self, start, end):
+        if self.board[end[0]][end[1]] == 'p':
+            return True
+        else:
+            return False
+        
+    def check_queen(self, start, end):
+        if self.board[end[0]][end[1]] == 'q':
+            return True
+        else:
+            return False
+        
+    def check_rook(self, start, end):
+        if self.board[end[0]][end[1]] == 'r':
+            return True
+        else:
+            return False
+        
+        
+        
